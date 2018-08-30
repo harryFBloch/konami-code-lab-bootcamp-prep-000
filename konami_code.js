cbda4d53ca944decaeaf86ea1bc2af36a63e8922
Harry Bloch <harryfbloch@gmail.com>
2018-08-30 14:18:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,35 @@ const codes = [
   "a"
 ];
 
+var index = 0;
+
+
 function init() {
-  // your code here
+
+  var body = document.getElementsByTagName("body")[0];
+  for (let i = 0; i < codes.length; i++){
+    addListener(codes[i], body)
+    //addListener('click', body)
+  }
+}
+
+function addListener(eventType, node){
+  console.log(node.events)
+  node.addEventListener(eventType, function(event) {
+    console.log("trigger")
+    checkIfEventIsCorrect(event);
+});
+}
+
+function checkIfEventIsCorrect(event){
+  console.log(event)
+  if(event === codes[index]){
+    if (index === codes.length - 1){
+      alert("You Found Me");
+    }else{
+      index++;
+    }
+  }else{
+    index = 0;
+  }
 }
